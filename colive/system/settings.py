0c58ea46581dd07f9b79517493d486bf77497df0
urkin-dev <yurkin.ivan5456@gmail.com>
2023-05-21 22:43:39 +0300

    update: token life

--- colive/system/settings.py
+++ colive/system/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
 }
 
 REST_FRAMEWORK = {
